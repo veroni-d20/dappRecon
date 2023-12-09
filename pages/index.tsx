262c9a8311416779edddd5942037ad228168fe6d
veroni-d20 <veronishwetha.23it@licet.ac.in>
2023-12-09 18:13:05 +0530

    search page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,64 @@
 import Layout from "@/components/layout";
+import Link from "next/link";
 
 export default function home() {
   return (
     <Layout pageTitle="Search Your dapp">
-      <div>home</div>
+      <div>
+        <div className="relative">
+          <input
+            type="text"
+            name="dapp-name"
+            id="dapp-name"
+            autoComplete="given-name"
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-400 sm:text-sm sm:leading-6"
+          />
+          <button
+            type="button"
+            className="absolute end-1 top-1/2 -translate-y-1/2 rounded-full bg-gray-50 p-2 text-gray-600 transition hover:text-gray-700"
+          >
+            <span className="sr-only">Search</span>
+            <svg
+              xmlns="http:www.w3.org/2000/svg"
+              className="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div className="mt-5">
+        <Link
+          href={"/register"}
+          className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2"
+        >
+          <svg
+            className="mx-auto h-12 w-12 text-gray-400"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 48 48"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
+            />
+          </svg>
+          <span className="mt-2 block text-sm font-semibold text-gray-900">
+            Dapp not found
+          </span>
+        </Link>
+      </div>
     </Layout>
   );
 }
